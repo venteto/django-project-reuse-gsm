94898fe2cdaf593dd011bbe5101f3341d1478254
Venteto <g0jtb5nre@relay.firefox.com>
2025-08-20 16:36:30 -0400

    tweak static files settings and add favicon

--- settings_base.py
+++ settings_base.py
@@ -12,32 +12,27 @@ environ.Env.read_env(BASE_DIR / '../sync/aux' / 'prod.env')
 environ.Env.read_env(BASE_DIR / '../sync/aux_reuse' / 'any.env')
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-
 ADMIN_PATH = env('DJANGO_ADMIN_PATH')
-
 PROJECT_NAME = env('DJANGO_PROJECT_NAME')
-
 ENV_NAME = env('DJANGO_ENV_NAME', default='prod')
 
 if ENV_NAME == 'dev':
-
     DEBUG = True
-
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-
     DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / '../local/db' / 'dev.sqlite3',}}
-
     ADMIN_HEADER_BG = '#008c00'  # darker green
-
 else:
-
     ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
-
     DATABASES = { 'default': env.db_url('DATABASE_URL'), }
-
     ADMIN_HEADER_BG = '#8c0000'  # darker red
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'd1_project' / 'static',
+    BASE_DIR / 'd2_gsm' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 # ------------------------------------------------------------------------------
 #           `startproject` remaining boilerplate
@@ -55,15 +50,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # ALLOWED_HOSTS = []
@@ -92,6 +84,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'd1_project.urls_root'
 
+# TEMPLATES[0]['DIRS'].append(BASE_DIR / 'd1_project' / 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -100,6 +93,7 @@ TEMPLATES = [
         # this will not work if you reuse settings as a PyPI package
         # it only works as a git submodule or subtree
         'DIRS': [
+            BASE_DIR / 'd1_project' / 'templates',
             BASE_DIR / 'd2_gsm' / 'templates',
         ],
 
