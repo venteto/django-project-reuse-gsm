243d3c786c49e1cbdb8ca63831e6b4162e1a4ca1
Venteto <g0jtb5nre@relay.firefox.com>
2025-08-20 17:22:09 -0400

    tweak favicon settings

--- urls_reuse.py
+++ urls_reuse.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage 
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+
+'''  admin/base_site.html override template also affects the title '''
+admin.site.site_title = 'REAL Django Admin'   # DEFAULT: "Django site admin"
+admin.site.site_header = 'Real Django Admin'  # DEFAULT: "Django Administration"
+admin.site.index_title = 'Site Admin Home'    # DEFAULT: "Site Administration"
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_PATH, admin.site.urls),
+
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url(
+        'favicons/favicon-32x32.png'), permanent=True)),
+
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
